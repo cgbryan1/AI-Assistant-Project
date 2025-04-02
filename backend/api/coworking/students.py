fa3312d8df34d5dec298890906bd2a27fbe0a106
Emma Coye <ecoye@unc.edu>
2025-04-02 13:53:52 -0400

    Created document for student api

---
+++ backend/api/coworking/students.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import Sequence
+from datetime import datetime
+
+__authors__ = ["Emma Coye, Manasi Chaudhary, Caroline Bryan, Kathryn Brown"]
+__copyright__ = "Copyright 2025"
+__license__ = "MIT"
