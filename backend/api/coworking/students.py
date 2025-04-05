e97ad9f2e62c03bb2729371e74a0d340a43624de
Caroline Bryan <143732576+cgbryan1@users.noreply.github.com>
2025-04-05 13:45:57 -0400

    Finalizing Backend Outline (#14)
    * outlining activeuser model and adding api route
    
    * ready to attempt service layer outline
    
    * Added service layer with suggested methods and further developed api route descriptions
    
    * first outline of story 1 routes
    
    * fixing routes and adding nts
    
    * adding return types on routes
    
    * signing off
    
    ---------
    
    Co-authored-by: Emma Coye <ecoye@unc.edu>

--- backend/api/coworking/students.py
+++ backend/api/coworking/students.py
@@ -2,19 +2,78 @@ from fastapi import APIRouter, Depends, HTTPException
 from typing import Sequence
 from datetime import datetime
 
+from backend.models.user_details import UserDetails
+
+
 __authors__ = ["Emma Coye, Manasi Chaudhary, Caroline Bryan, Kathryn Brown"]
 __copyright__ = "Copyright 2025"
 __license__ = "MIT"
 
-#  separate router file for B1's user story 1 (sally student checking friends)
+# separate router file for B1's user story 1 (sally student checking friends)
 
 api = APIRouter(prefix="/api/coworking")
 
 
 @api.get(
-    "/students?name={name-id}",  # TODO: double check
+    "/students/{pid}",  # TODO: double check pid vs onyen
     summary="Get active specified user",
     description=" Checking whether a specifed user is active and visible in the XL. ",
+    responses={
+        200: {
+            "description": "User returned!",
+        },
+        307: {
+            "description": "Redirected.",
+        },
+        404: {
+            "description": "Users not found.",
+        },
+    },
+    tags=["Coworking"],
+)  # TODO add in response model, fix response codes
+def check_user_activity(pid: int) -> bool:  # appearing in /docs yay! # TODO
+    try:
+        # existing method to get a user's active reservations: _get_active_reservations_for_user()
+        return True  # call service here
+    except:
+        raise HTTPException(status_code=404, detail="No active user could be found.")
+
+
+# Route based on course
+@api.get(
+    "/student?course={course_id}",
+    summary="Get active users in a specified course.",
+    description=" TODO ",
+    responses={
+        200: {
+            "description": "Classmates returned!",
+        },
+        307: {
+            "description": "Redirected.",
+        },
+        404: {
+            "description": "Classmates not found.",
+        },
+    },
+    tags=["Coworking"],
+)
+def get_active_classmates(
+    course_id: str,
+) -> list[UserDetails]:  # TODO check course_id right way to access?
+    try:
+        # course_id is input, route for
+        # todo create fake data to test with
+        return []  # call service here
+    except:
+        raise HTTPException(
+            status_code=404, detail="No active classmates could be found."
+        )
+
+
+@api.get(
+    "/students/active",
+    summary="Get all active users in CSXL",
+    description=" TODO ",
     responses={
         200: {
             "description": "Users returned!",
@@ -27,14 +86,12 @@ api = APIRouter(prefix="/api/coworking")
         },
     },
     tags=["Coworking"],
-)  # TODO add in response model, fix response codes, add description
-def get_active_users():  # appearing in /docs yay!
+)
+def get_active_users() -> list[UserDetails]:  # TODO
     try:
-        return 0  # call service here
+        # use get_active_reservations and filter here?
+        return None  # call service here
+        # todo create fake data to test with
+
     except:
-        raise HTTPException(status_code=404, detail="Active users not found")
-
-
-# Route based on course
-
-# Route based on course section
+        raise HTTPException(status_code=404, detail="No active users could be found")
